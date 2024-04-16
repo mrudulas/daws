31ded0867fbb037a4897ea33c01d7aa70bc733e5
devops <smruduladev@gmail.com>
2024-04-16 10:41:16 +0530

    day1 changes

---
+++ commandlineargs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+PERSON1 = $[1];
+PERSON2 = $[2];
+
+echo "$PERSION1 IS SAYING HI TO $PERSON2"
---
+++ variables.sh
@@ -0,0 +1,4 @@
+PERSON1=SIVA
+PERSON2=KARTHIKEYA
+echo "$PERSION1 ::: hello $PERSON2, how are you"
+echo "$PERSION2 ::: hello $PERSON1, how are you"
