6e7ca3cc35b233048cc8214a40343dfe70c8d737
devops <smruduladev@gmail.com>
2024-04-16 12:28:11 +0530

    practice

--- specialvariables.sh
+++ specialvariables.sh
@@ -1,8 +1,22 @@
 echo "All variables from command line argumants ::: $@"
 echo "Number of variables from command line argumants ::: $#"
 
+echo "Current script file name  ::: $PWD"
+
 echo "Current working directory ::: $PWD"
 
 echo "CUrrent loggedin user ::: $USER"
 
-echo "Current process id ::: $PID"
+echo "Current process id ::: $$"
+
+echo "Previous process id ::: $!"
+
+echo "Hostname ::: $HOSTNAME"
+
+
+# echo "Home directory of current user: $HOME"
+# echo "Which user is running this script: $USER"
+# echo "Hostname: $HOSTNAME"
+# echo "Process ID of the current shell script: $$"
+# sleep 60 &
+# echo "Process ID of last background command: $!"
