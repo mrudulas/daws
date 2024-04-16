2b06cd9e6d1b4a51b18cae424d2f2b567b4380b6
devops <smruduladev@gmail.com>
2024-04-16 11:50:11 +0530

    practice

--- first.sh
+++ first.sh
@@ -1,4 +1,4 @@
-#! bin/bash
+#! /bin/bash
 # first is called shibang is the location interpretroe, comands written inside the shell script 
 #sh <script> || bash <script>
 echo "Hi this is my first script";
--- variables.sh
+++ variables.sh
@@ -1,3 +1,4 @@
+#! /bin/bash
 PERSON1=SIVA
 PERSON2=KARTHIKEYA
 echo "$PERSION1 ::: hello $PERSON2, how are you"
