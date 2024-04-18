2403661ddfc3a6c2dea11a60d2a27c3b5f1d1799
devops <smruduladev@gmail.com>
2024-04-18 07:55:20 +0530

    dfaf

--- logs.sh
+++ logs.sh
@@ -3,17 +3,18 @@
 USERID=$(id -u)
 TIMESTAMP=$(date +%f-%h-%m-%s)
 SCRIPTNAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/SCRIPTNAME
+LOGFILE=/tmp/SCRIPTNAME-TIMESTAMP.log
 
+echo "log file :::: $LOGFILE"
 VALIDATE(){
-    if [ $1 -ne 0 ]
+    if [ $1 -eq 0 ]
     then
-        echo "Exist Status :  $2 success "
+        echo -e "Exist Status : \e[31m $2 success "
         exit 1
     else
-        echo "Exist Status :  $2 failure "
+        echo -e "Exist Status : \e[32m $2 failure "
     fi
 }
 
-dnf install mysql -y&>>LOGFILE
+dnf install mysql -y&>>$LOGFILE
 VALIDATE $? "mysql install"
