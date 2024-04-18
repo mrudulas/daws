16c50543953dc37e8316d384d43daa3f255f2a07
devops <smruduladev@gmail.com>
2024-04-18 07:48:18 +0530

    ss

--- logs.sh
+++ logs.sh
@@ -6,11 +6,13 @@ SCRIPTNAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/SCRIPTNAME
 
 VALIDATE(){
-    if[$1 -ne 0]
+    if [ $1 -ne 0 ]
     then
         echo "Exist Status :  $2 success "
+        exit 1
     else
         echo "Exist Status :  $2 failure "
+    fi
 }
 
 dnf install mysql -y&>>LOGFILE
