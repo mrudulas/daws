908c8832ef1eb6a207f5f546aa64c44b64ff1ff5
devops <smruduladev@gmail.com>
2024-04-23 07:43:57 +0530

    scripts added

--- expense-project/mysql.sh
+++ expense-project/mysql.sh
@@ -1,37 +1,10 @@
-#!/bin/bash
+source ./common.sh
 
-USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPTNAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
-
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
+check_root()
 
 echo "Please enter DB password:"
 # enter abc123 as password
 read -s mysql_root_password
-
-echo "log file :::: $LOGFILE"
-
-
-if [ $USERID -ne 0 ]
-then echo "Please use root access to install"
-  exit 1
-else echo "user has access"
-fi
-
-VALIDATE(){
-    if [$1 -ne 0]
-    then
-        echo -e "$2 ......... $R FIALURE $N"
-        exit 1
-    else
-        echo -e "$2 ......... $G SUCCESS $N"
-    fi
-}
 
 dnf install mysql-server -y &>>$LOGFILE
 VALIDATE $? "My sql server installing"
