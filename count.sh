75b1265749ba54bfa0f3724171a1baf4cd792173
devops <smruduladev@gmail.com>
2024-04-24 14:32:34 +0530

    sds

--- count.sh
+++ count.sh
@@ -11,11 +11,10 @@ totalwords=$(wc -w < example.txt)
 echo "total words: $totalwords"
 
 
-# read -a WORDS -d "" < example.txt
-# echo ${WORDS}
-# for word in "${WORDS[@]}"
-# do 
-#     echo $word
-# done
+read -a WORDS -d "" < example.txt
+for word in "${WORDS[@]}"
+do 
+    echo "$word"
+done
 
-awk '{ for (i=1; i<=NF; i++) print $i }' "example.txt" | sort | uniq -c | sort -nr
+awk '{ for (i=1; i<=NF; i++) print $i }' "example.txt" | sort | uniq -c | sort -nr | head -n 5
