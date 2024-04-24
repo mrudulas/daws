e0ceaf6b96ef96a817f8030513464cabd5609ad9
devops <smruduladev@gmail.com>
2024-04-24 20:30:14 +0530

    sdsd

--- cpu.sh
+++ cpu.sh
@@ -3,4 +3,13 @@
 # Use ps command to list processes and their CPU usage, then sort them based on CPU usage
 # Use head command to display the top 5 processes
 
-ps -eo pid,ppid,cmd,%cpu,%mem --sort=-%cpu | head -n 6
+PROCESSLIST=$(ps -eo pid,ppid,cmd,%cpu,%mem --sort=-%cpu | head -n 6)
+THRESHOLD=0.1
+
+MESSAGE=""
+
+while IFS= read -r process
+do
+USAGE=$(echo $process | awk -F " " 'print $4F}')
+echo $USAGE
+done <<< $PROCESSLIST
