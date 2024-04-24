9df82fdde9acd22e9d0990275900cba99ab0884a
devops <smruduladev@gmail.com>
2024-04-24 14:35:05 +0530

    sasa

--- count.sh
+++ count.sh
@@ -14,7 +14,19 @@ echo "total words: $totalwords"
 read -a WORDS -d "" < example.txt
 for word in "${WORDS[@]}"
 do 
-    echo "$word"
+ if [ -n "$word" ]
+ then
+            # Increment the count of the word in the associative array
+            ((word_count["$word"]++))
+ fi
+done
+counter=0
+for word in "${!word_count[@]}"; do
+    printf "%-10s : %d\n" "$word" "${word_count[$word]}"
+    ((counter++))
+    if [ $counter -eq 5 ]; then
+        break
+    fi
 done
 
 awk '{ for (i=1; i<=NF; i++) print $i }' "example.txt" | sort | uniq -c | sort -nr | head -n 5
