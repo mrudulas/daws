d724ec26b2c6f18e6d27d446aef8c20553f32765
devops <smruduladev@gmail.com>
2024-04-24 13:31:34 +0530

    sdfdsf

--- expense-project/backend.sh
+++ expense-project/backend.sh
@@ -1,36 +1,12 @@
 #!/bin/bash
 
-USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPTNAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
+source ./common.sh
 
+check_root
 echo "Please enter DB password:"
 read -s mysql_root_password
 
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
 
-echo "log file :::: $LOGFILE"
-
-
-if [ $USERID -ne 0 ]
-then echo "Please use root access to install"
-  exit 1
-else echo "user has access"
-fi
-
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$2 ......... $R FIALURE $N"
-        exit 1
-    else
-        echo -e "$2 ......... $G SUCCESS $N"
-    fi
-}
 
 dnf module disable nodejs -y &>>$ 
 VALIDATE $? "disabling nodejs version................."
